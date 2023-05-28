609d3bcc80252e984aac0ce784d66a608e77561b
Vitaly Kovalenko <rtm_wrk@rambler.ru>
2023-05-28 22:46:45 +0300

    Add correctly test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,9 +1,6 @@
 package ru.netology.rest;
 
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
@@ -12,21 +9,15 @@ public class PostmanEchoTest {
     @Test
     void shouldPostmanEchoTest() {
         // Given - When - Then
-        String requestBody = "{ \"command\": {\n" +
-                "    \"firstName\": \"Ivan\",\n" +
-                "    \"lastName\": \"Petrov\",\n" +
-                "    \"age\": 26\n" +
-                "  }\n" +
-                "}";
+        String userData = "{\"id\":1,\"name\":\"Elias\",\"job\":\"Engineer\"}";
 
         given()
                 .baseUri("https://postman-echo.com")
-                .body(requestBody)
+                .body(userData)
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .contentType(ContentType.JSON)
-                .body("command.age", equalTo("26"));
+                .body("data", equalTo(userData));
     }
 }
