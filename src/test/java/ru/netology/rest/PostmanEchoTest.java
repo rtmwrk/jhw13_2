0d7f9f2366bb15da095f5cee36e4a13aef591fec
Vitaly Kovalenko <rtm_wrk@rambler.ru>
2023-05-28 14:39:02 +0300

    Crash test Poastman Echo unknown resons

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,24 +1,32 @@
 package ru.netology.rest;
 
-import io.restassured.http.ContentType;
-import org.hamcrest.Matchers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class PostmanEchoTest {
+
     @Test
     void shouldPostmanEchoTest() {
         // Given - When - Then
+        String requestBody = "{ \"command\": {\n" +
+                "    \"firstName\": \"Ivan\",\n" +
+                "    \"lastName\": \"Petrov\",\n" +
+                "    \"age\": 26\n" +
+                "  }\n" +
+                "}";
+
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data")
+                .body(requestBody)
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(200)
-                //.body(/* --> ваша проверка здесь <-- */)
-        ;
+                .contentType(ContentType.JSON)
+                .body("command.age", equalTo("26"));
     }
 }
